e8d3a1452895fa72f3a645e2698a29534e8adbcf
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-09-29 19:09:06 +0500

    Task 3

---
+++ Tyuiu.PankovaAA.Sprint1.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.PankovaAA.Sprint1.Task3.V11.Lib;
+namespace Tyuiu.PankovaAA.Sprint1.Task3.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpress()
+        {
+            DataService ds = new DataService();
+            double x1 = -2;
+            double y1 = -5;
+            double x2 = 1;
+            double y2 = 7;
+            double x3 = 5;
+            double y3 = -3;
+            var res = ds.TriangleArea(x1, y1, x2, y2, x3, y3);
+            Assert.AreEqual(-39,res);
+
+        }
+    }
+}
---
+++ Tyuiu.PankovaAA.Sprint1.Task3.V11/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.PankovaAA.Sprint1.Task3.V11.Lib;
+namespace Tyuiu.PankovaAA.Sprint1.Task3.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 |Выполнила: Панькова А.А.| ПИНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  Спринт #1                                                              *");
+            Console.WriteLine("*  Тема: Операторы составного присваивания                                *");
+            Console.WriteLine("*  Задание #03                                                            *");
+            Console.WriteLine("*  Вариант #11                                                            *");
+            Console.WriteLine("*  Выполнила: Панькова Ангелина Алексеевна    |ПИНб-25-1|                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  УСЛОВИЕ:                                                               *");
+            Console.WriteLine("*  Написать программу, которая запрашивает у пользователя исходные данные *");
+            Console.WriteLine("*   выполняет указанные расчёты и печатает результат на экране.           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*  РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите координаты треугольника:");
+
+            Console.Write("x1 = ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y1 = ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("x2 = ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y2 = ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("x3 = ");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("y3 = ");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+
+      
+
+            Console.WriteLine("Площадь треугольника:" + ds.TriangleArea(x1,y1,x2,y2,x3,y3));
+
+        }
+    }
+}
