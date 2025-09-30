4cc0f2c161d05ed85ea42a2c286281f9dd2f0fd7
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-09-30 22:26:36 +0500

    Task 7

--- Tyuiu.PankovaAA.Sprint1.Task7.V9.Lib/DataService.cs
+++ Tyuiu.PankovaAA.Sprint1.Task7.V9.Lib/DataService.cs
@@ -5,8 +5,12 @@ namespace Tyuiu.PankovaAA.Sprint1.Task7.V9.Lib
     {
         public double Calculate(double x, double y)
         {
-            double res = (Math.Exp(x) - (Math.Pow(y,2) + Math.Cos(Math.Pow(x,3)) + 12 * x * y - 3 * Math.Pow(x,2) / (Math.Cos(Math.Pow(x, 3)+3) + 18 * x * y - 1)));
-            return res;
+            return Math.Exp(x) - (
+    (Math.Pow(y, 2) + Math.Cos(Math.Pow(x, 3)) + 12 * x * y - 3 * Math.Pow(x, 2))
+    /
+    (Math.Cos(Math.Pow(x, 3) + 3) + 18 * y - 1)
+);
+
         }
     }
 }
